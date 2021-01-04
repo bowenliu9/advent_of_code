484a006621d41992b0861170b6d4e222d70b9f06
Bowen Liu <bowenl520@gmail.com>
2021-01-03 23:42:39 -0800

    Day15 part 2 work in progress, Day16 Part 1 successfully completed

--- Day15.py
+++ Day15.py
@@ -6,27 +6,33 @@ def last_occurance(spoken, number):
 		if spoken[index] == number:
 			return index 
 
-a = [2,15,0,9,1,20]
-spoken = []
-counter = 0
-prev = 0
+def turns(a,iteration):
+	spoken = []
+	counter = 0
+	prev = 0
 
-while counter < 2020:
-	if counter < len(a):
-		# read out the given list first and store it in spoken
-		spoken.append(a[counter])
-		prev = a[counter]
-	elif prev not in spoken[0:-1]:
-		# check if it's the first time the number is said
-		spoken.append(0)
-		prev = 0
-	else:
-		# if it's been spoken, find the index last time prev was spoken
-		num = counter - 1 - last_occurance(spoken,prev)
-		spoken.append(num)
-		prev = num
-	counter += 1
+	while counter < iteration:
+		if counter < len(a):
+			# read out the given list first and store it in spoken
+			spoken.append(a[counter])
+			prev = a[counter]
+		elif prev not in spoken[0:-1]:
+			# check if it's the first time the number is said
+			spoken.append(0)
+			prev = 0
+		else:
+			# if it's been spoken, find the index last time prev was spoken
+			num = counter - 1 - last_occurance(spoken,prev)
+			spoken.append(num)
+			prev = num
+		counter += 1
 
-print(spoken[-1])
+	print(spoken[-1])
+	return spoken[-1]
+
+a = [0,3,6]
+turns(a,2020)
+turns(a,30000000)
+
 
 # Part 2
